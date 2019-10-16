44c5669bb5b477cc9c0563e7baac4fd2877e6c41
Zsailer <zachsailer@gmail.com>
2019-10-15 21:29:44 -0700

    working jhub service with pycasbin

--- jupyter_authorized_server/app.py
+++ jupyter_authorized_server/app.py
@@ -5,18 +5,44 @@ from jupyterhub.services.auth import HubAuth
 
 from traitlets import Unicode
 
+import casbin
 
 class AuthorizedServerApp(HubAuth, ServerApp):
+
     name = 'jhubshare'
     description = _("""The Jupyter Server with authorization""")
     port = 9999
     default_url = "/jhubshare"
-    api_token=Unicode(config=True)
+    api_token=Unicode('model.conf', config=True)
     base_url = '/services'
     default_services = []
     extra_services = [
         'jupyter_authorized_server.contents'
     ]
+    model_file = Unicode('model.conf', config=True)
+    policy_file = Unicode('policy.csv', config=True)
+
+    def initialize_enforcer(self):
+        self.enforcer = casbin.Enforcer(self.model_file, self.policy_file)
+        self.tornado_settings['enforcer'] = self.enforcer
+
+    def initialize(self, argv=None, load_extensions=False):
+        super(ServerApp, self).initialize(argv)
+        self.init_logging()
+        if self._dispatching:
+            return
+        self.init_configurables()
+        if load_extensions:
+            self.init_server_extension_config()
+        self.init_components()
+        self.initialize_enforcer()
+        self.init_webapp()
+        self.init_terminals()
+        self.init_signal()
+        if load_extensions:
+            self.init_server_extensions()
+        self.init_mime_overrides()
+        self.init_shutdown_no_activity()
 
 
 main = AuthorizedServerApp.launch_instance
